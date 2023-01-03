ee81c10f02cda5de6e364ad785b551bae8b628a7
Viktor.Ryabovol <victor.ryabovol@artpix3d.com>
2023-01-03 22:36:57 +0200

    add postgress + sequelize

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const routes = require('./router');
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: 'localhost',
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+});
 
 const PORT = process.env.PORT || 5000;
 
@@ -14,4 +21,17 @@ app.use(bodyParser.json());
 
 app.use(routes);
 
-app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+const connectToDB = async () => {
+    console.log("connection...");
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+    connectToDB();
+});
