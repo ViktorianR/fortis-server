d2fee88901f56a724fa28c3b87056e96c61667a3
Viktor.Ryabovol <victor.ryabovol@artpix3d.com>
2023-01-04 18:29:55 +0200

    move into db logic

--- index.js
+++ index.js
@@ -2,16 +2,11 @@ require('dotenv').config();
 
 const cors = require('cors');
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const routes = require('./router');
-const { Sequelize } = require('sequelize');
+const sequelize = require('./db');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: 'localhost',
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
-});
+const app = express();
 
 const PORT = process.env.PORT || 5000;
 
@@ -21,9 +16,10 @@ app.use(bodyParser.json());
 
 app.use(routes);
 
-const connectToDB = async (callback) => {
+const connectToDB = async () => {
     await sequelize.authenticate();
-    callback();
-}
+    await sequelize.sync();
+    console.log(`Server is running on ${PORT}`);
+};
 
-app.listen(PORT, () => connectToDB(() => console.log(`Server is running on ${PORT}`)));
+app.listen(PORT, () => connectToDB());
